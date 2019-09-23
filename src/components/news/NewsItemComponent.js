a48f3343453cd42e0a29b71176820ea66b645e31
Jorge Vazquez <jorge.vazquez@mercadolibre.com>
2019-09-23 17:45:01 -0300

    Styling the list some

--- src/components/news/NewsItemComponent.js
+++ src/components/news/NewsItemComponent.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const styles = {
   div: {
-    padding: '5px',
-    borderBottom: '1px dashed #ccc'
+    padding: '20px 10px 20px 10px',
+    borderBottom: '1px solid #ddd'
   },
   h1: {
-    fontSize: '14px'
+    fontSize: '17px',
+    fontFamily: 'Georgia, serif',
+    color: '#1A1A1A'
+  },
+  h1Link: {
+    color: '#1A1A1A'
   },
   content: {
     fontSize: '12px'
@@ -18,7 +23,7 @@ const NewsItemComponent = (props) => {
 
   return (
     <div style={styles.div}>
-      <h1 style={styles.h1}><a target="_blank" href={url}>{title.toString()}</a></h1>
+      <h1 style={styles.h1}><a style={styles.h1Link} target="_blank" href={url}>{title.toString()}</a></h1>
       <div style={styles.content}>{desc.toString()}</div>
     </div>
   );
